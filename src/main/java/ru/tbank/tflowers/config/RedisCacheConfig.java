ce7ba401a11b4f9b72de522d679ffa2d30423cb8
Konstantin Polshchikov <polshchikov.k@yandex.ru>
2024-11-19 09:45:56 +0300

    Включил typing для object mapper, но все падает, так как он ожидает получить тип, но для верхнего массива его не написало из-за toList.

--- src/main/java/ru/tbank/tflowers/config/RedisCacheConfig.java
+++ src/main/java/ru/tbank/tflowers/config/RedisCacheConfig.java
@@ -1,6 +1,12 @@
 package ru.tbank.tflowers.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -40,6 +46,13 @@ public class RedisCacheConfig {
     }
 
     private ObjectMapper getObjectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.activateDefaultTyping(
+                objectMapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+        return objectMapper;
     }
 }
